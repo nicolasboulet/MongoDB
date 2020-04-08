199822ef8eb45b5c396353e1720a5c0db06d7bba
Nicolas Boulet <nicolasboulet@live.fr>
2020-04-08 15:57:47 +0200

    Create basics.rb

---
+++ basics.rb
@@ -0,0 +1,22 @@
+require 'mongo'
+
+# Define log level to INFO (default DEBUG)
+Mongo::Logger.logger.level = ::Logger::INFO
+
+# Connect to localhosts
+db = Mongo::Client.new('mongodb://localhost:27017')
+
+# use database test
+db = db.use('test')
+
+# What is the name of database
+print "Database name is : "
+p db.database.name
+
+# What are the collections names
+print "The collections are : "
+p db.database.collection_names
+
+# Show first entry
+puts "The first entry is :"
+p db[:zips].find.first
